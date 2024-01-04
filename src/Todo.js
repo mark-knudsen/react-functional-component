62aad6be3d4ff0a8898c39c14c6f31b029f90827
mark-knudsen <marksvendstrupknudsen@gmail.com>
2024-01-05 00:10:54 +0100

    refactor Todo as a file Todo.js

---
+++ src/Todo.js
@@ -0,0 +1,77 @@
+import "./App.css";
+import React, { useState } from "react";
+
+const Todo = () => {
+  const [todos, settodos] = useState([]);
+  const [value, setvalue] = useState("");
+  const [editing, setediting] = useState(false);
+  const [currentid, setcurrentid] = useState("");
+  const [currentValue, setcurrentValue] = useState("");
+  const [currentTodo, setcurrentTodo] = useState(null);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (editing) {    // If editing, update the existing todo   
+      onEditTodo(currentid, currentValue);
+      setediting(false);
+    } else {    // If not editing, add a new todo
+      const obj = {
+        name: value,
+        id: Date.now(),
+      };
+      if (value !== "") {
+        settodos(todos.concat(obj));
+        setvalue("");
+      }
+    }
+  };
+
+  const onChange = (e) => {
+    setvalue(e.target.value);
+  };
+
+  const onEditTodo = (id, newValue) => {
+    settodos((prevTodos) =>
+      prevTodos.map((todo) => (todo.id === id ? { ...todo, name: newValue } : todo))
+    );
+  };
+
+  const onToggleEdit = (todo) => {
+    setediting(true);
+    setcurrentValue(todo.name);
+    setcurrentid(todo.id);
+    setcurrentTodo(todo);
+  };
+
+  const onEditInputChange = (e) => {
+    setcurrentValue(e.target.value);
+  };
+
+  const onDeleteTask = (itemId) => {
+    settodos(todos.filter((id) => id.id !== itemId));
+  };
+
+  const mylist = todos.map((todo) => (
+    <li key={todo.id} className="todo_item">
+      {todo.name}
+      <button className="edit" onClick={() => onToggleEdit(todo)}>Edit</button>
+      <button className="remove" onClick={() => onDeleteTask(todo.id)}>Remove</button>
+    </li>
+  ));
+
+  return (
+    <div className="App">
+      <form onSubmit={onSubmit}>
+        <input
+          placeholder={editing ? "Edit your task" : "Type your task"}
+          value={editing ? currentValue : value}
+          onChange={editing ? onEditInputChange : onChange}
+        />
+        <button className="add" type="submit">{editing ? "Update ToDo" : "Add ToDo"}</button>
+      </form>
+      <ul className="todo_wrapper">{mylist}</ul>
+    </div>
+  );
+};
+
+export default Todo;
